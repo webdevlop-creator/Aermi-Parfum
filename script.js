8fe2a82e670e9ee33cc8c50010da3ba0a0586cbe
webdevlop-creator <websitedev58@gmail.com>
2026-01-03 22:22:14 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
-function openDetail(nama, deskripsi, harga) {
-  document.getElementById("detail").style.display = "block";
-  document.getElementById("namaParfum").innerText = nama;
-  document.getElementById("deskripsiParfum").innerText = deskripsi;
-  document.getElementById("hargaParfum").innerText = "Harga: Rp" + harga + " (30ml)";
+function openDetail(id) {
+  document.getElementById(id).style.display = "block";
+}
 
-  const nomorWA = "6285187337263"; 
-
-  const pesan = `Halo, saya mau pesan ${nama} ukuran 30ml dengan harga Rp${harga}`;
-
-  document.getElementById("waBtn").href =
-    "https://wa.me/" + nomorWA + "?text=" + encodeURIComponent(pesan);
+function closeDetail() {
+  document.querySelectorAll(".modal").forEach(m => {
+    m.style.display = "none";
+  });
 }
